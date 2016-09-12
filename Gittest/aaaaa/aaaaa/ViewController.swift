314bb4d02773f2b4ca930d2de43e090e295d2bdd
herolsl <Sven@Sven-iMac.local>
2016-09-12 18:11:44 +0800

    22222

---
+++ Gittest/aaaaa/aaaaa/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  aaaaa
+//
+//  Created by Sven Liu on 16/9/12.
+//  Copyright © 2016年 Sven Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    func printHello(name : String) {
+        
+        print("Hello \(name), this is master");
+    }
+    
+}
+
